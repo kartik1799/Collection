341b178d8aaeff506c35ff08b2f5654f481e9d4c
Kartik Goel <64986352+kartik1799@users.noreply.github.com>
2021-06-23 14:09:34 +0530

    Add files via upload

---
+++ MathOperator.java
@@ -0,0 +1,10 @@
+public class MathOperator {
+    /* Implementation of Math Operators*/
+
+    public static void main(String args[]){
+        int prod1=10*20;
+        int prod2=prod1*5;
+        int prod3=prod1*prod2;
+        System.out.println(prod3);
+    }
+}
