7a4ba175a2e63d9732208fd1457f4a57ef4d16f8
Kartik Goel <64986352+kartik1799@users.noreply.github.com>
2021-06-29 17:16:08 +0530

    Add files via upload

--- stringManipulation.java
+++ stringManipulation.java
@@ -4,6 +4,7 @@ package StringExercise;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 
 public class stringManipulation
 {
@@ -21,7 +22,21 @@ public class stringManipulation
 
             firstNonRepeating();
 
-            divideAndReverse();
+            frequent();
+
+            removeFromAnotherString();
+
+            reverseWords();
+
+            reverseEveryWord();
+
+            removePAndLA();
+
+            countDuplicates();
+
+            rotationString();
+
+            endsWithString();
         }
         catch(Exception e)
         {
@@ -124,7 +139,7 @@ public class stringManipulation
 
             String s2 = "This is exercise 1";
 
-            int num = s1.compareTo(s2);
+            int num = s1.compareToIgnoreCase(s2);
 
             if (num < 0) {
                 System.out.println("\"" + s1 + "\"" + " is less than " + "\"" + s2 + "\"");
@@ -592,5 +607,339 @@ public class stringManipulation
             e.printStackTrace();
         }
     }
+    public static void frequent()
+    {
+        try
+        {
+            ArrayList<Character> arr=new ArrayList<Character>();
+
+            String s="successes";
+
+            for(int i=0;i<s.length();i++)
+            {
+                if(!arr.contains(s.charAt(i))){
+                arr.add(s.charAt(i));
+                }
+            }
+            ArrayList<Integer> arr1=new ArrayList<Integer>();
+
+            ArrayList<Character> arr2=new ArrayList<Character>();
+
+            for(int i=0;i<arr.size();i++)
+            {
+                int count=0;
+
+                for(int j=0;j<s.length();j++)
+                {
+                    if(s.charAt(j)==arr.get(i))
+                    {
+                        count+=1;
+                    }
+                }
+                arr1.add(count);
+
+                arr2.add(arr.get(i));
+            }
+
+            int max=Collections.max(arr1);
+
+            for(int i=0;i<arr1.size();i++)
+            {
+             if(arr1.get(i)==max)
+             {
+                 System.out.println(arr2.get(i));
+
+                 break;
+             }
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void removeFromAnotherString()
+    {
+        try {
+            String s1 = "This is String Manipulation";
+
+            String s2 = "knot";
+
+            char arr[] = new char[s1.length()];
+
+            char[] mask = new char[256];
+
+            for (int i = 0; i < s2.length(); i++) {
+                mask[s2.charAt(i)]++;
+            }
+
+            for (int i = 0; i < s1.length(); i++)
+            {
+                if (mask[s1.charAt(i)] == 0)
+                {
+                    System.out.print(s1.charAt(i));
+                }
+            }
+            System.out.println("\n");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void secondMostFrequent()
+    {
+        try
+        {
+            ArrayList<Character> arr=new ArrayList<Character>();
+
+            String s="successes";
+
+            for(int i=0;i<s.length();i++)
+            {
+                if(!arr.contains(s.charAt(i))){
+                    arr.add(s.charAt(i));
+                }
+            }
+            ArrayList<Integer> arr1=new ArrayList<Integer>();
+
+            ArrayList<Character> arr2=new ArrayList<Character>();
+
+            for(int i=0;i<arr.size();i++)
+            {
+                int count=0;
+
+                for(int j=0;j<s.length();j++)
+                {
+                    if(s.charAt(j)==arr.get(i))
+                    {
+                        count+=1;
+                    }
+                }
+                arr1.add(count);
+
+                arr2.add(arr.get(i));
+            }
+            ArrayList<Integer> a1=(ArrayList<Integer>)arr1.clone();
+
+            Collections.sort(arr1);
+
+            int n=arr1.get(arr1.size()-2);
+
+            for(int i=0;i<a1.size();i++)
+            {
+                if(a1.get(i)==n)
+                {
+                    System.out.println(arr2.get(i));
+
+                    break;
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void reverseWords()
+    {
+        try
+        {
+            String str="This is String Manipulation";
+
+            String[] s=str.split(" ");
+
+            for(int i=s.length-1;i>=0;i--)
+            {
+                System.out.print(s[i]+" ");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void reverseEveryWord()
+    {
+        try {
+            System.out.println("\n");
+
+            String str = "This is String Manipulation";
+
+            String[] s = str.split(" ");
+
+            for (String i:s)
+            {
+                StringBuffer c=new StringBuffer(i);
+
+                System.out.print(c.reverse()+" ");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void removePAndLA()
+    {
+        try
+        {
+            System.out.println("\n");
+
+            String str="Manipulation";
+
+            char[] arr=str.toCharArray();
+
+            for(int i=0;i<arr.length;i++)
+            {
+                if (arr[i]=='p')
+                {
+                    continue;
+                }
+                else if(i+1<arr.length && arr[i]=='l' && arr[i+1]=='a')
+                {
+                    ++i;
+                }
+                else
+                {
+                    System.out.print(arr[i]);
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void countDuplicates()
+    {
+        try
+        {
+            System.out.println("\n");
+
+            String str="Manipulation";
+
+            char[] chr=str.toCharArray();
+
+            String s1="";
+
+            for(char s:chr)
+            {
+                if(s1.indexOf(s)==-1)
+                {
+                    s1=s1+s;
+                }
+            }
+
+            int[] arr=new int[256];
+
+            char c;
+
+            for(int i=0;i<str.length();i++)
+            {
+                c=str.charAt(i);
+
+                ++arr[c];
+            }
+            for(int i=0;i<s1.length();i++)
+            {
+                if(arr[s1.charAt(i)]>1)
+                {
+                    System.out.println(s1.charAt(i)+" appears: "+arr[s1.charAt(i)]+" times");
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void rotationString()
+    {
+        try
+        {
+           String s="ABCDA";
+
+           String s1="CDAAB";
+
+           String concat=s+s;
+
+           if(s.length()==s1.length() && concat.contains(s1))
+           {
+               System.out.println("They are rotations of each other");
+           }
+           else
+           {
+               System.out.println("They are not rotations of each other");
+           }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void endsWithString()
+    {
+        try {
+            System.out.println("\n");
+
+            String str = "This is String Manipulation";
+
+            if(str.endsWith("no"))
+            {
+                System.out.println("Yes");
+            }
+            else
+            {
+                System.out.println("No");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void startWithString()
+    {
+        try {
+            System.out.println("\n");
+
+            String str = "This is String Manipulation";
+
+            if(str.startsWith("This"))
+            {
+                System.out.println("Yes");
+            }
+            else
+            {
+                System.out.println("No");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void cutAndAppend()
+    {
+        try {
+            System.out.println("\n");
+
+            String str1 = "String";
+
+            String str2="Manipulation";
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
 
 }
